a27a5e269de48a1da964a522872986bda11802ed
Ahmed Issa <57608414+AhmedIssa11@users.noreply.github.com>
2020-11-08 21:26:43 +0200

    Add files via upload

---
+++ app/configs/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// App Link
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://localhost/CS314/');
+define('APPNAME', 'CS314');
+
+
+// Create Database Info
+
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASSWORD","");
+define("DB_NAME","phone shop");
