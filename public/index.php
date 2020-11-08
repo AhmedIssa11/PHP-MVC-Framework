2f982163b8557c739f59bc051f4f52b2e37d9910
Ahmed Issa <57608414+AhmedIssa11@users.noreply.github.com>
2020-11-08 21:20:37 +0200

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once '../app/loader.php';
+include '../app/views/inc/header.inc.php';
+include '../app/views/inc/footer.inc.php';
+
+$core = new Core();
+/*
+    Auto Load Classes 
+*/
+
+// spl_autoload_register(function($class) {
+
+//     require 'classes/' . $class . '.php';
+
+// });
+
+
+//echo "Hello Wolrd!";
+// echo "<pre>"; echo print_r($obj); echo "</pre>";
+
+?>
